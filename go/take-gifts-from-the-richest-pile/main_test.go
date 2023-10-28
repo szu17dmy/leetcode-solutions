a4e73135b63948ebb0cbb70584e53dbb9cea2dec
domain <domain@localhost.localdomain>
2023-10-28 14:58:41 +0800

    Add go/take-gifts-from-the-richest-pile

---
+++ go/take-gifts-from-the-richest-pile/main_test.go
@@ -0,0 +1,39 @@
+package take_gifts_from_the_richest_pile
+
+import "testing"
+
+func Test_pickGifts(t *testing.T) {
+	type args struct {
+		gifts []int
+		k     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "Case 1",
+			args: args{
+				gifts: []int{25, 64, 9, 4, 100},
+				k:     4,
+			},
+			want: 29,
+		},
+		{
+			name: "Case 2",
+			args: args{
+				gifts: []int{1, 1, 1, 1},
+				k:     4,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pickGifts(tt.args.gifts, tt.args.k); got != tt.want {
+				t.Errorf("pickGifts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
