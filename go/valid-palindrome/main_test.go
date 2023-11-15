b56c4119431d25b187e37f99d5b7e9097215f1a5
domain <domain@localhost.localdomain>
2023-11-15 15:44:14 +0800

    Add go/valid-palindrome

---
+++ go/valid-palindrome/main_test.go
@@ -0,0 +1,43 @@
+package valid_palindrome
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Case 1",
+			args: args{
+				s: "A man, a plan, a canal: Panama",
+			},
+			want: true,
+		},
+		{
+			name: "Case 2",
+			args: args{
+				s: "race a car",
+			},
+			want: false,
+		},
+		{
+			name: "Case 3",
+			args: args{
+				s: " ",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
