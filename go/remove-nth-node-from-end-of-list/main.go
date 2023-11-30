a37eba3aa05c812676c56849b52bbf7416c06d82
domain <do@my-sh.dev>
2023-11-30 14:58:01 +0800

    Add go/remove-nth-node-from-end-of-list

---
+++ go/remove-nth-node-from-end-of-list/main.go
@@ -0,0 +1,30 @@
+package remove_nth_node_from_end_of_list
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	fh := &ListNode{
+		Next: head,
+	}
+	fast, slow := fh, fh
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return fh.Next
+}
