88b28850efb6cfb734e4709730d71931429441d5
domain <do@my-sh.dev>
2024-02-21 14:46:27 +0800

    Add go/coin-change.

---
+++ go/coin-change/main.go
@@ -0,0 +1,24 @@
+package coin_change
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
+		for j := 0; j < len(coins); j++ {
+			if coins[j] <= i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
+		}
+	}
+	if dp[amount] == amount+1 {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ go/coin-change/main_test.go
@@ -0,0 +1,47 @@
+package coin_change
+
+import "testing"
+
+func Test_coinChange(t *testing.T) {
+	type args struct {
+		coins  []int
+		amount int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Case 1",
+			args: args{
+				coins:  []int{1, 2, 5},
+				amount: 11,
+			},
+			want: 3,
+		},
+		{
+			name: "Case 2",
+			args: args{
+				coins:  []int{2},
+				amount: 3,
+			},
+			want: -1,
+		},
+		{
+			name: "Case 3",
+			args: args{
+				coins:  []int{1},
+				amount: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := coinChange(tt.args.coins, tt.args.amount); got != tt.want {
+				t.Errorf("coinChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
